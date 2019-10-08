9511a1de4d22ca11c15b566d00da8637267df76c
Pablo Saavedra <psaavedra@igalia.com>
2019-10-08 19:59:11 +0200

    Add the timeout-factor parameter in browserperfdash-benchmark
    The timeout-factor is useful to run the tests in slower devices where
    the default timeout value defined in the .plan files is not high enough
    to let the tests end.

--- webkitpy/benchmark_runner/benchmark_runner.py
+++ webkitpy/benchmark_runner/benchmark_runner.py
@@ -23,7 +23,7 @@ _log = logging.getLogger(__name__)
 class BenchmarkRunner(object):
     name = 'benchmark_runner'
 
-    def __init__(self, plan_file, local_copy, count_override, build_dir, output_file, platform, browser, scale_unit=True, show_iteration_values=False, device_id=None):
+    def __init__(self, plan_file, local_copy, timeout_factor, count_override, build_dir, output_file, platform, browser, scale_unit=True, show_iteration_values=False, device_id=None):
         try:
             plan_file = self._find_plan_file(plan_file)
             with open(plan_file, 'r') as fp:
@@ -35,6 +35,8 @@ class BenchmarkRunner(object):
                     self._plan['local_copy'] = local_copy
                 if count_override:
                     self._plan['count'] = count_override
+                if timeout_factor:
+                    self._plan['timeout'] = self._plan['timeout'] * timeout_factor
                 self._browser_driver = BrowserDriverFactory.create(platform, browser)
                 self._build_dir = os.path.abspath(build_dir) if build_dir else None
                 self._output_file = output_file
